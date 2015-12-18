9e415e8aab99633b6e89dba2f5c8dd1b6e148da4
chuckcoughlin <chuck_coughlin@comcast.net>
2015-12-18 11:26:27 -0700

    Initial upload.

---
+++ workspace/AI_Client/src/com/ils/client/ClientHook.java
@@ -0,0 +1,15 @@
+package com.ils..client;
+
+import com.inductiveautomation.factorypmi.application.runtime.AbstractClientModuleHook;
+import com.inductiveautomation.ignition.client.model.ClientContext;
+import com.inductiveautomation.ignition.common.licensing.LicenseState;
+
+public class ClientHook extends AbstractClientModuleHook {
+
+	@Override
+	public void startup(ClientContext context, LicenseState activationState)
+			throws Exception {
+		// TODO write code here!
+	}
+	
+}
---
+++ workspace/AI_Gateway/src/com/ils/gateway/GatewayHook.java
@@ -0,0 +1,25 @@
+package com.ils..gateway;
+
+import com.inductiveautomation.ignition.common.licensing.LicenseState;
+import com.inductiveautomation.ignition.gateway.model.AbstractGatewayModuleHook;
+import com.inductiveautomation.ignition.gateway.model.GatewayContext;
+
+public class GatewayHook extends AbstractGatewayModuleHook {
+
+	@Override
+	public void setup(GatewayContext context) {
+		// TODO Write code, override methods as needed
+
+	}
+
+	@Override
+	public void shutdown() {
+
+	}
+
+	@Override
+	public void startup(LicenseState licenseState) {
+
+	}
+
+}
