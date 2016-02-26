cc7fc8e4e9ad7b94ca04bdc20d5602de82e44524
chuckcoughlin <chuck_coughlin@comcast.net>
2016-02-26 08:04:33 -0700

    Update to module page.

--- workspace/ApplicationInstaller-gateway/src/com/ils/ai/gateway/panel/ModuleStep.java
+++ workspace/ApplicationInstaller-gateway/src/com/ils/ai/gateway/panel/ModuleStep.java
@@ -3,6 +3,7 @@ package com.ils.ai.gateway.panel;
 import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
@@ -29,6 +30,12 @@ public class ModuleStep extends InstallWizardStep {
 			protected void populateItem(ListItem<String> item) {
                 String text = (String) item.getModelObject();
                 item.add(new Label("text", text));
+            }
+        });
+        
+        add(new Button("doit") {
+            public void onSubmit() {
+                info("Doit was pressed!");
             }
         });
     }
